6722a073559f56260d0107ab575bce6718476d86
Luke Blazewski <luke.blazewski@sofomo.com>
2021-11-11 22:50:54 +0100

    Loading models

--- src/composables/idSystem/idSystem.ts
+++ src/composables/idSystem/idSystem.ts
@@ -1,4 +1,5 @@
 import { ModelMetaData } from "./interfaces/ModelMetaData.interface";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 export class IdSystem {
   private _metaData: ModelMetaData;
@@ -16,11 +17,15 @@ export class IdSystem {
     return this._metaData;
   }
 
-  public load(): Promise<void> {
-    throw new Error("Method not implemented.");
+  public async load(): Promise<void> {
+    const loader = new GLTFLoader();
+    const modelGltf = await loader.loadAsync(this._metaData.webGlModelPath);
+    this._model = modelGltf.scene;
   }
 
   public purge(): void {
-    throw new Error("Method not implemented.");
+    //TODO: Here utilize scene.traverse(this goes through each object and returns it)
+    // check for all of neccessary stuff from https://threejsfundamentals.org/threejs/lessons/threejs-cleanup.html and dispose them
+    // https://dustinpfister.github.io/2021/06/03/threejs-object3d-traverse/index.html
   }
 }
