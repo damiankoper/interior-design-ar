0c0ffe63f112a81b19301b167e076304a7632ce0
dex1g <dexopl@gmail.com>
2021-11-18 00:18:00 +0100

    display model name on a tile

--- src/components/ModelTile.vue
+++ src/components/ModelTile.vue
@@ -1,14 +1,17 @@
 <template>
-  <div @click="openModelDetails()">
-    <el-image fit="contain" :src="imageSrc"></el-image>
-    <el-button
-      @click.stop="startAR"
-      :class="{ hidden: !isXrSupported }"
-      type="primary"
-      plain
-    >
-      <font-awesome-icon :icon="['fas', 'vr-cardboard']" />
-    </el-button>
+  <div class="model-tile" @click="openModelDetails()">
+    <el-image fit="contain" :src="imageSrc" />
+    <div class="label">
+      <p>{{ model.getModelMetaData().name }}</p>
+      <el-button
+        @click.stop="startAR"
+        :class="{ hidden: !isXrSupported }"
+        type="primary"
+        plain
+      >
+        <font-awesome-icon :icon="['fas', 'vr-cardboard']" />
+      </el-button>
+    </div>
   </div>
 </template>
 
@@ -46,9 +49,32 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-div {
+.model-tile {
   cursor: pointer;
   position: relative;
+
+  .label {
+    width: calc(100% - 20px);
+    background-color: rgba(255, 255, 255, 0.507);
+    position: absolute;
+    bottom: 0;
+    padding: 10px 10px 14px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    p {
+      margin: auto;
+      flex: 1;
+      font-weight: 700;
+    }
+
+    .el-button {
+      padding: 6px 10.25px;
+      margin: auto;
+      height: 40px;
+    }
+  }
 
   .el-image {
     width: 100%;
@@ -59,13 +85,6 @@ div {
   .el-image:hover {
     box-shadow: 0 2px 4px 2px rgba(0, 0, 0, 0.12),
       0 0 6px 8px rgba(0, 0, 0, 0.04);
-  }
-
-  .el-button {
-    position: absolute;
-    bottom: 5%;
-    right: 5%;
-    padding: 6px 10.25px;
   }
 
   .hidden {
